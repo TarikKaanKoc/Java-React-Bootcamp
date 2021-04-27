42738d1909ac30afe0af86ae851c858808ce77b5
Tarık Kaan Koç <83168207+TarikKaanKoc@users.noreply.github.com>
2021-04-27 04:00:26 +0300

    Delete Lesson2 directory

--- Lesson2/ders2/src/module-info.java
+++
@@ -1,2 +0,0 @@
-module ders2 {
-}
--- Lesson2/oopİntro/src/oopİntro/ProductManager.java
+++
@@ -1,7 +0,0 @@
-package oop›ntro;
-
-public class ProductManager {
-	public void addToCart(Product product) {
-		System.out.println("Sepete eklendi: "+ product.name);
-	}
-}
