ded50869538accaac17634e38ce857ed3052ff1f
Tarık Kaan Koç <83168207+TarikKaanKoc@users.noreply.github.com>
2021-04-28 02:09:48 +0300

    Add files via upload

---
+++ HomeWork1 - Day2/src/Category.java
@@ -0,0 +1,15 @@
+
+public class Category {
+
+	int id;
+	String branch;
+	String Instructor;
+	
+	public Category(int id, String branch, String Instructor) { 
+		
+		this.id=id;
+		this.branch=branch;
+		this.Instructor=Instructor;
+		
+	}
+}
---
+++ HomeWork1 - Day2/src/CourseManager.java
@@ -0,0 +1,14 @@
+
+public class CourseManager {
+
+	// iþ sýnýfý claasý
+	public void addToCourse(Course course ) { 
+		System.out.println(course.courseName+" "+ "Eklendi");
+	}
+	
+	public void finishCourse(Course course ) { 
+		System.out.println(course.courseName+" "+ "Baþarýyla Tamamlandý");
+	}
+	
+	
+}
