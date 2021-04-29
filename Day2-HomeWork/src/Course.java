17d5e0030f23554e9b12dbff68bc13fc2392a832
Tarık Kaan Koç <83168207+TarikKaanKoc@users.noreply.github.com>
2021-04-29 16:06:57 +0300

    Day 2 - HomeWork(1)

---
+++ Day2-HomeWork/src/Course.java
@@ -0,0 +1,13 @@
+public class Course {
+
+    int CourseId;
+    String CourseName;
+    String CourseTeacher;
+
+    public Course(int CourseId, String CourseName, String CourseTeacher ) {
+        this.CourseId = CourseId;
+        this.CourseName = CourseName;
+        this.CourseTeacher = CourseTeacher;
+
+    }
+}
