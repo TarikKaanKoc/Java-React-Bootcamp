aad69d58a086d6e6369912de142040c002c2b2b8
Tarık Kaan Koç <83168207+TarikKaanKoc@users.noreply.github.com>
2021-04-29 16:13:47 +0300

    Day 2  - Homework(1)

---
+++ Day2-Homework/src/Category.java
@@ -0,0 +1,13 @@
+public class Category {
+
+    int courseId;
+    String detail;
+    String coursePrice;
+
+    public  Category(int courseId,String detail,String coursePrice){
+        this.courseId=courseId;
+        this.detail=detail;
+        this.coursePrice=coursePrice;
+    }
+
+}
