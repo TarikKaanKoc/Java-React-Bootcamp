490dee611289a161a150a14522c713b93429cf2c
Tarık Kaan Koç <83168207+TarikKaanKoc@users.noreply.github.com>
2021-05-02 01:51:26 +0300

    Delete Homwork2-Day3 directory

--- Homwork2-Day3/src/StudentManager.java
+++
@@ -1,9 +0,0 @@
-public class StudentManager {
-
-    public void add(Student student ){
-
-        System.out.println(student.getFirstName()+"Öğrenci eklendi.");
-
-    }
-
-}
--- Homwork2-Day3/src/UserManager.java
+++
@@ -1,7 +0,0 @@
-public class UserManager {
-
-    public void add(User user ){
-        System.out.println("kullanıcı eklendi");
-    }
-
-}
