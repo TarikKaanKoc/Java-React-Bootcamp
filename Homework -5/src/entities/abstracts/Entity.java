52c2a8c1ae91685c245899cba2f5438cc6e35de5
Tarık Kaan Koç <83168207+TarikKaanKoc@users.noreply.github.com>
2021-05-08 14:03:04 +0300

    Delete Homework -5 directory

--- Homework -5/src/entities/abstracts/Entity.java
+++
@@ -1,4 +0,0 @@
-package entities.abstracts;
-
-public interface Entity {
-}
--- Homework -5/src/googleApi/GoogleApiService.java
+++
@@ -1,7 +0,0 @@
-package googleApi;
-
-public class GoogleApiService {
-    public void sign(String mail, String password){
-        System.out.println("Signed with google api services - "+ mail);
-    }
-}
