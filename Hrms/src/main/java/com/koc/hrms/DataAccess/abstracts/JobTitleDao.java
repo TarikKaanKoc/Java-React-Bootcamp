a27ddd3fa45a7b17f43b5b291fcb9ae0bce30080
Tarık Kaan Koç <83168207+TarikKaanKoc@users.noreply.github.com>
2021-05-26 11:13:23 +0300

    Delete Hrms directory

--- Hrms/src/main/java/com/koc/hrms/DataAccess/abstracts/JobTitleDao.java
+++
@@ -1,7 +0,0 @@
-package com.koc.hrms.DataAccess.abstracts;
-
-import com.koc.hrms.Entities.concretes.JobTitle;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobTitleDao extends JpaRepository<JobTitle,Integer> {
-}
